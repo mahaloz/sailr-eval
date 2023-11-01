9e7665e04564b9ffcdaa36d83603eb498d0d6f34
mahaloz <zion@zionbasque.com>
2023-10-31 20:32:06 -0700

    Add a CI for verifying pipeline still works

--- scripts/ci_verify_pipeline.sh
+++ scripts/ci_verify_pipeline.sh
@@ -6,6 +6,9 @@ EXAMPLE_PROJECT_DIR="$SCRIPT_DIR/../tests/example_project"
 RESULTS_DIR="$SCRIPT_DIR/../results/O2"
 CORES="$(nproc --all)"
 
+echo "Installing dependencies..."
+sudo apt-get update && sudo apt-get install -y gcc make strip
+
 echo "[+] Checking if results directory exists..."
 if [ ! -d "$RESULTS_DIR" ]; then
     echo "[+] Creating results directory..."
@@ -15,18 +18,18 @@ fi
 echo "[+] Running pipeline in $PROJECT_DIR..."
 cd "$PROJECT_DIR" || exit 1
 
-echo "[+] The following runs will require sudo for docker and file permissions..."
 echo "[+] Running compilation pipeline..." && \
 # runs on the default opts, which is O2
-sudo ./docker-eval.sh --compile example_project --cores "$CORES" && \
+./eval.py --compile example_project --cores "$CORES" && \
 echo "[+] Running decompilation pipeline (only for source)..." && \
 ./eval.py --decompile example_project --cores "$CORES" --use-dec source && \
-echo "[+] Running decompilation pipeline (for other decompilers)..." && \
-sudo ./docker-eval.sh --decompile example_project --use-dec angr_sailr angr_dream --cores "$CORES" && \
+echo "[+] Copying decompilation..." && \
+mkdir -p "$PROJECT_DIR"/results/O2/example_project/sailr_decompiled && \
+cp "$PROJECT_DIR"/tests/ci/angr* "$PROJECT_DIR"/results/O2/example_project/sailr_decompiled && \
 echo "[+] Running measurement pipeline (gotos, bools, calls, cfged)..." && \
 ./eval.py --measure example_project --use-metric gotos cfged bools func_calls --use-dec source angr_sailr angr_dream --cores "$CORES" && \
 echo "[+] Running aggregation pipeline..." && \
 ./eval.py --summarize-targets example_project --use-dec source angr_sailr angr_dream --use-metric gotos cfged bools func_calls --show-stats && \
 # cleanup
-#rm -rf "$RESULTS_DIR/example_project" && \
+rm -rf "$RESULTS_DIR/example_project" && \
 echo "[+] The pipeline has successfully finished!" || (echo "[!] Pipeline failed, check the last stage it was in to figure out where!" && exit 1)
